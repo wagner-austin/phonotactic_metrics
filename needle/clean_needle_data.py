1318d701fd7052d71c7ed147eb8f6bfa287f701d
Arya K <aryarksub@gmail.com>
2024-01-18 12:45:50 -0800

    Add lmer model outcomes for Needle data

---
+++ needle/clean_needle_data.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+dir = "needle/"
+
+df = pd.read_csv(f"{dir}needle_data.csv")
+
+# Map DISC to CMU representation of sounds
+
+disc_to_cmu = dict()
+
+for index, row in df.iterrows():
+    sounds = row["cmu"].split()
+    for i in range(len(row["disc"])):
+        char = row["disc"][i]
+        disc_to_cmu[char] = sounds[i]
+
+# For training data, convert sounds from DISC to CMU, and write to new file
+lines = []
+with open(f'{dir}needle_orig_training.txt', 'r') as file:
+    for line in file:
+        representations = line.strip().split(",")
+        cmu_repr = [disc_to_cmu[char] for char in representations[1]]
+        lines.append(' '.join(cmu_repr))
+with open(f'{dir}needle_cleaned_training.txt', 'w') as file:
+    for line in lines:
+        file.write(line + "\n")
+
+# Create test file
+df["cmu"].to_csv(f"{dir}needle_test_data.csv", header=False, index=False)
+
+# Clean metric output csv
+df_metric = pd.read_csv(f"{dir}needle_original_metric_output.csv")
+df_metric.replace(float('-inf'), -50, inplace=True)
+if "subject" not in df_metric.columns:
+    df_metric.insert(loc=0, column="subject", value=df["subjID"])
+df_metric["rating"] = df["rating"]
+df_metric.to_csv(f"{dir}needle_cleaned_metric_output.csv", index=False)
---
+++ needle/phonotactics_needle.R
@@ -0,0 +1,51 @@
+library(readr)
+library(tidyverse)
+library(lme4)
+library(lmerTest)
+
+setwd("C:/hplaptop/4thYearUCI/PhonotacticsResearch/needle")
+
+needle_data <- read_csv("needle_cleaned_metric_output.csv")
+needle_data[4:17] = scale(needle_data[4:17])
+
+uni_bi_model = lmer(rating ~ uni_prob * bi_prob + (1|word) + (1|subject), data=needle_data)
+summary(uni_bi_model)
+
+pos_uni_bi_model = lmer(rating ~ pos_uni_score * pos_bi_score + (1|word) + (1|subject), data = needle_data)
+summary(pos_uni_bi_model)
+
+pos_uni_bi_smoothed_model = lmer(rating ~ pos_uni_score_smoothed * pos_bi_score_smoothed + 
+                                     (1|word) + (1|subject), data = needle_data)
+summary(pos_uni_bi_smoothed_model)
+
+uni_bi_smoothed_model = lmer(rating ~ uni_prob * bi_prob_smoothed + (1|word) + (1|subject),
+                             data = needle_data)
+summary(uni_bi_smoothed_model)
+
+model_comp = anova(uni_bi_model, pos_uni_bi_model, pos_uni_bi_smoothed_model, uni_bi_smoothed_model)
+model_comp
+
+all_models = list(uni_bi_model, pos_uni_bi_model, pos_uni_bi_smoothed_model, uni_bi_smoothed_model)
+
+
+num_models = length(all_models)
+
+fixed_effects = sapply(all_models, FUN=fixef)
+
+intercept = vector(mode="numeric", length=num_models)
+uni_coef = vector(mode="numeric", length=num_models)
+bi_coef = vector(mode="numeric", length=num_models)
+interaction_coef = vector(mode="numeric", length=num_models)
+
+for (index in 1:num_models) {
+    intercept[index] = fixed_effects[(index-1) * num_models + 1]
+    uni_coef[index] = fixed_effects[(index-1) * num_models + 2]
+    bi_coef[index] = fixed_effects[(index-1) * num_models + 3]
+    interaction_coef[index] = fixed_effects[(index-1) * num_models + 4]
+}
+
+df = data.frame(intercept, uni_coef, bi_coef, interaction_coef, model_comp[2], model_comp[3], model_comp[4])
+df
+
+sorted_df = df[order(df$AIC),]
+sorted_df
