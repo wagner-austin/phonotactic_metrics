d47984ce1aafe27dd9d75e4daf603bb12b668499
Arya K <aryarksub@gmail.com>
2024-03-08 00:36:36 -0800

    Change ordinal regression to linear regression for Daland data

--- daland/phonotactics_daland.R
+++ daland/phonotactics_daland.R
@@ -10,32 +10,32 @@ setwd("C:/hplaptop/4thYearUCI/PhonotacticsResearch/daland")
 daland_data <- read_csv("daland_cleaned_metric_output.csv")
 daland_data[3:16] = scale(daland_data[3:16])
 
-uni_bi_model = polr(as.factor(rating) ~ uni_prob * bi_prob, data=daland_data, Hess=TRUE)
+uni_bi_model = glm(rating ~ uni_prob * bi_prob, data=daland_data)
 summary(uni_bi_model)
 
-pos_uni_bi_model = polr(as.factor(rating) ~ pos_uni_score * pos_bi_score, data = daland_data, Hess=TRUE)
+pos_uni_bi_model = glm(rating ~ pos_uni_score * pos_bi_score, data = daland_data)
 summary(pos_uni_bi_model)
 
-pos_uni_bi_smoothed_model = polr(as.factor(rating) ~ pos_uni_score_smoothed * pos_bi_score_smoothed, 
-                                 data = daland_data, Hess=TRUE)
+pos_uni_bi_smoothed_model = glm(rating ~ pos_uni_score_smoothed * pos_bi_score_smoothed, 
+                                 data = daland_data)
 summary(pos_uni_bi_smoothed_model)
 
-uni_bi_smoothed_model = polr(as.factor(rating) ~ uni_prob * bi_prob_smoothed, data = daland_data, Hess=TRUE)
+uni_bi_smoothed_model = glm(rating ~ uni_prob * bi_prob_smoothed, data = daland_data)
 summary(uni_bi_smoothed_model)
 
-uni_bi_freq_model = polr(as.factor(rating) ~ uni_prob_freq_weighted * bi_prob_freq_weighted, data=daland_data, Hess=TRUE)
+uni_bi_freq_model = glm(rating ~ uni_prob_freq_weighted * bi_prob_freq_weighted, data=daland_data)
 summary(uni_bi_freq_model)
 
-uni_bi_freq_smooth_model = polr(as.factor(rating) ~ uni_prob_freq_weighted * bi_prob_freq_weighted_smoothed,
-                             data=daland_data, Hess=TRUE)
+uni_bi_freq_smooth_model = glm(rating ~ uni_prob_freq_weighted * bi_prob_freq_weighted_smoothed,
+                             data=daland_data)
 summary(uni_bi_freq_smooth_model)
 
-pos_uni_bi_freq_model = polr(as.factor(rating) ~ pos_uni_score_freq_weighted * pos_bi_score_freq_weighted,
-                             data=daland_data, Hess=TRUE)
+pos_uni_bi_freq_model = glm(rating ~ pos_uni_score_freq_weighted * pos_bi_score_freq_weighted,
+                             data=daland_data)
 summary(pos_uni_bi_freq_model)
 
-pos_uni_bi_freq_smoothed_model = polr(as.factor(rating) ~ pos_uni_score_freq_weighted_smoothed * pos_bi_score_freq_weighted_smoothed,
-                                      data=daland_data, Hess=TRUE)
+pos_uni_bi_freq_smoothed_model = glm(rating ~ pos_uni_score_freq_weighted_smoothed * pos_bi_score_freq_weighted_smoothed,
+                                      data=daland_data)
 summary(pos_uni_bi_freq_smoothed_model)
 
 all_model_names = vector()
@@ -49,30 +49,23 @@ for (var in ls()) {
 
 num_models = length(all_model_names)
 
-intercept1_2 = vector(mode="numeric", length=num_models)
-intercept2_3 = vector(mode="numeric", length=num_models)
-intercept3_4 = vector(mode="numeric", length=num_models)
-intercept4_5 = vector(mode="numeric", length=num_models)
-uni_coef = vector(mode="numeric", length=num_models)
+intercept = vector(mode="numeric", length=num_models)
 uni_coef = vector(mode="numeric", length=num_models)
 bi_coef = vector(mode="numeric", length=num_models)
 interaction_coef = vector(mode="numeric", length=num_models)
 AIC = vector(mode="numeric", length=num_models)
 
 for (index in 1:num_models) {
-    model = get(all_model_names[index])
-    intercept1_2[index] = model[[2]][1]
-    intercept2_3[index] = model[[2]][2]
-    intercept3_4[index] = model[[2]][3]
-    intercept4_5[index] = model[[2]][4]
-    uni_coef[index] = coef(model)[1]
-    bi_coef[index] = coef(model)[2]
-    interaction_coef[index] = coef(model)[3]
-    AIC[index] = AIC(model)
+    coef_index = 30*(index-1) + 1
+    aic_index = 30*(index-1) + 11
+    intercept[index] = all_models[[coef_index]][1]
+    uni_coef[index] = all_models[[coef_index]][2]
+    bi_coef[index] = all_models[[coef_index]][3]
+    interaction_coef[index] = all_models[[coef_index]][4]
+    AIC[index] = all_models[[aic_index]]
 }
 
-df = data.frame(intercept1_2, intercept2_3, intercept3_4, intercept4_5, uni_coef, 
-                bi_coef, interaction_coef, AIC, row.names=all_model_names)
+df = data.frame(intercept, uni_coef, bi_coef, interaction_coef, AIC, row.names=all_model_names)
 df
 
 sorted_df = df[order(df$AIC),]
